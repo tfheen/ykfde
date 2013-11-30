f879d635bf64aa9054012c84aa8c13e332d596ea
Klaus Umbach <klaus-github@uxix.de>
2013-11-30 22:39:18 +0100

    added ykfdectl

--- README.md
+++ README.md
@@ -8,17 +8,30 @@ YubiKey Full Disk Encryption
 Quick start:
 ------------
 
-First install the package, then do something like the following:
+First install the package, then do something like the following (with your
+yubikey plugged in!)
 
-    # uuidgen > /boot/yubikey-challenge
-    # ykchalresp -2 $(cat /boot/yubikey-challenge)
-    # cryptsetup luksAddKey /dev/sda2 <(ykchalresp -2 $(cat /boot/yubikey-challenge))
+    # ykfdectl new
 
 On bootup, you will be asked to insert a yubikey (2.2 or newer) which
 will then provide the response.  If you do not want to use a yubikey,
 press enter and then enter a normal passphrase during bootup.
 
+Change the key
+--------------
+
+    # ykfdectl update
+
+Changing the key on each reboot
+-------------------------------
+
+This is still very basic.
+
+    # ln -s /usr/sbin/ykfdectl /etc/init.d/ykfdectl
+    # ln -s /etc/init.d/ykfdectl /etc/rc2.d/S99ykfdectl
+
+
 Limitations/bugs:
 -----------------
 * uses only slot 2 ykchalresp settings, meaning no support for slot 1
-* does not update the challenge after each boot.
+* might need more error-handling.
---
+++ ykfde.sh
@@ -0,0 +1,90 @@
+#!/bin/bash -e
+
+# set the defaults
+YKOPTS="-2"
+LUKS_DEVICE="/dev/sda2"
+LUKS_SLOT="7"
+CHALLENGE_FILE="/boot/yubikey-challenge"
+
+
+# read defaults..
+if [ -e /etc/default/ykfde ]; then
+    source /etc/default/ykfde
+fi
+
+
+function get_new_challenge () { # {{{
+    if [ -z ${CHALLENGE} ]; then
+        export CHALLENGE="$(base64 < /dev/urandom | head -c 64)"
+        if ykinfo $YKOPTS >/dev/null ; then
+            echo -ne "${CHALLENGE}" > ${CHALLENGE_FILE}.new
+            chmod 400 ${CHALLENGE_FILE}.new
+        else
+            exit 1
+        fi
+    fi
+} # }}}
+
+function get_old_challenge () { # {{{
+    if [ -z ${OLD_CHALLENGE} ]; then
+        export OLD_CHALLENGE="$(cat ${CHALLENGE_FILE})"
+        mv ${CHALLENGE_FILE} ${CHALLENGE_FILE}.old
+    fi
+} # }}}
+
+function add_new_key () { # {{{
+    cryptsetup luksAddKey \
+        --key-slot ${LUKS_SLOT} ${LUKS_DEVICE} \
+        <(ykchalresp ${YKOPTS} ${CHALLENGE})
+
+    if [ "${?}" == "0" ]; then
+        mv  ${CHALLENGE_FILE}.new ${CHALLENGE_FILE}
+    fi
+} # }}}
+
+function update_key () { # {{{
+    OLD_KEY="$(ykchalresp ${YKOPTS} ${OLD_CHALLENGE})"
+
+    cryptsetup luksChangeKey \
+        --key-slot ${LUKS_SLOT} ${LUKS_DEVICE} \
+        --key-file <( echo "${OLD_KEY}" ) <(ykchalresp ${YKOPTS} ${CHALLENGE})
+
+    if [ "${?}" == "0" ]; then
+        mv  ${CHALLENGE_FILE}.new ${CHALLENGE_FILE} && \
+        rm  -f ${CHALLENGE_FILE}.old
+    fi
+
+} # }}}
+
+function print_help () { # {{{
+
+    echo "Usage: $0 (new|update|start|stop)"
+
+} # }}}
+
+case $1 in
+    new)
+        echo -ne "getting new key..."
+        get_new_challenge && echo "OK"
+        echo -ne "adding new key..."
+        add_new_key && echo "OK"
+        /usr/sbin/update-initramfs -u -k "$(uname -r)"
+
+    ;;
+    update|start)
+        echo -ne "getting keys..."
+        get_new_challenge && echo -ne '.'
+        get_old_challenge && echo 'OK'
+        echo -ne "changing key..."
+        update_key && echo 'OK'
+        /usr/sbin/update-initramfs -u -k "$(uname -r)"
+    ;;
+    stop)
+        echo "Nothing to do"
+    ;;
+    *)
+        print_help
+    ;;
+esac
+
+
